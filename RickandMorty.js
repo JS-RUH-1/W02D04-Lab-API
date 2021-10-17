7b9325962d660af22f3f390ccea6cd64ae3b1760
عبدالله الصبي <Abdullahb.alsabi@gmail.com>
2021-10-17 10:08:03 +0300

    update css

--- RickandMorty.js
+++ RickandMorty.js
@@ -24,21 +24,3 @@ fetch("https://rickandmortyapi.com/api/character", {
   .catch((err) => {
     console.log("rejected", err);
   });
-
-// function load() {
-//   let mydata = JSON.parse(data.results);
-//   //   console.log(mydata);
-
-//   let main = document.getElementById("card-group");
-//   for (let i = 0; i < mydata.length; i++) {
-//     const element = mydata[i];
-//     main.innerHTML += `
-//           <div class="card">
-//             <img class="card-img-top" src="${element.image}" alt=" image" />
-//             <div class="card-body">
-//               <h5 class="card-title">${element.name}</h5>
-//             </div>
-//           </div>
-//           `;
-//   }
-// }
